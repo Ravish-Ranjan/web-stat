67d11751b303f12f7f94924c587651de7f6c8c57
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com.>
2026-01-01 00:00:00 +0530

    FIX : changed page title on email verification page

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import Verifier from "./Verifier";
+import { metadata } from "../layout";
+
+metadata.title = "Verify Email | WebStat"
 
 function page() {
 	return (
